9f19340903eadf691a685a0703f5db0cd9f39d19
bast-i <m.bastug@me.com>
2016-04-24 20:29:29 +0200

    init

--- remote_ins.php
+++ remote_ins.php
@@ -1,5 +1,7 @@
 <?php
 
+// init the resource
+$ch = curl_init();
 
 $postData = array(
     'login' => '<admin></admin>',
@@ -8,6 +10,7 @@ $postData = array(
     'testcookie' => '1'
 );
 
+// set options
 curl_setopt_array($ch, array(
     CURLOPT_URL => 'http://152.62.101.224/index.php?signIn=1',
     CURLOPT_RETURNTRANSFER => true,
@@ -16,8 +19,11 @@ curl_setopt_array($ch, array(
     CURLOPT_FOLLOWLOCATION => true
 ));
 
+// execute
 $output = curl_exec($ch);
 echo $output;
 
+// free
+curl_close($ch);
 
 ?>
