c7d05bd625b93cd366f6883b33adb760d6974024
bast-i <m.bastug@me.com>
2016-04-24 20:17:08 +0200

    init

--- remote_ins.php
+++ remote_ins.php
@@ -1,19 +1,23 @@
 <?php
 
-$client = new \GuzzleHttp\Client();
-
-$response = $client->post('http://152.62.101.224/index.php?signIn=1', [
-    'body' => [
-        'admin' => $username,
-        'admin' => $password,
-        'action' => 'login'
-    ],
-    'cookies' => true
-]
+<?php
+$postData = array(
+    'login' => '<admin></admin>',
+    'pwd' => 'admin',
+    'redirect_to' => 'http://152.62.101.224/',
+    'testcookie' => '1'
 );
 
-$xml = $response;
-echo $xml;
+curl_setopt_array($ch, array(
+    CURLOPT_URL => 'http://152.62.101.224/index.php?signIn=1',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $postData,
+    CURLOPT_FOLLOWLOCATION => true
+));
+
+$output = curl_exec($ch);
+echo $output;
 
 
 ?>
